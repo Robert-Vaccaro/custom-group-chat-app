a57750df6c53a8b70f1a27de9caaea489f1fae4e
robertvaccaro1 <rvaccaro@zagmail.gonzaga.edu>
2019-12-24 12:46:02 -0800

    added heading

--- script.js
+++ script.js
@@ -22,3 +22,9 @@ $(document).ready(()=>{
         }
     });
 });
+
+if ( navigator.platform.indexOf('Win') != -1 ) {
+    window.document.getElementById("wrapper").setAttribute("class", "windows");
+  } else if ( navigator.platform.indexOf('Mac') != -1 ) {
+    window.document.getElementById("wrapper").setAttribute("class", "mac");
+  }
